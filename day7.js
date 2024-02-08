85711b6ecc6127d87cba226bbcd68f6594755a3d
Dhruv Jha <omdhruvjha@gmail.com>
2024-02-08 11:03:34 +0530

    day7

---
+++ day7.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+/**
+ * Express middleware to log incoming requests
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next function
+ */
+function requestLoggerMiddleware(req, res, next) {
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  console.log(`${timestamp} - ${method} request recieved`);
+}
+
+app.use(requestLoggerMiddleware);
